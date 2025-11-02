cacaaeb5b3f2d692032c7ecb540c9cdb62de2ce6
ryulkim <hobby519@naver.com>
2025-11-03 01:15:21 +0900

    refactor(Message): 열거형 상수 이름을 SNAKE_CASE로 변경

--- src/main/java/lotto/common/Message.java
+++ src/main/java/lotto/common/Message.java
@@ -1,9 +1,8 @@
 package lotto.common;
 
 public enum Message {
-    INPUTMONEY("구입금액을 입력해 주세요."),
-    INPUTPRIZENUMBERS("당첨 번호를 입력해 주세요.")
-    ;
+    INPUT_MONEY("구입금액을 입력해 주세요."),
+    INPUT_PRIZE_NUMBERS("당첨 번호를 입력해 주세요.");
 
     private final String message;
 
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,27 +1,27 @@
 package lotto.view;
 
-import static lotto.common.Message.INPUTMONEY;
-import static lotto.common.Message.INPUTPRIZENUMBERS;
+import static lotto.common.Message.INPUT_MONEY;
+import static lotto.common.Message.INPUT_PRIZE_NUMBERS;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.util.Print;
 
 public class InputView {
-    public static void inputMoney(){
-        Print.printMessage(INPUTMONEY);
+    public static void inputMoney() {
+        Print.printMessage(INPUT_MONEY);
         readLine();
     }
 
-    public static void inputPrizeNumbers(){
-        Print.printMessage(INPUTPRIZENUMBERS);
+    public static void inputPrizeNumbers() {
+        Print.printMessage(INPUT_PRIZE_NUMBERS);
         readLine();
     }
 
-    public static void close(){
+    public static void close() {
         Console.close();
     }
 
-    private static String readLine(){
+    private static String readLine() {
         return Console.readLine();
     }
 }
