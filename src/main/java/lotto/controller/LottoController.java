ca8f251736074fcdfc6744e1468937042cac3bcd
ryulkim <hobby519@naver.com>
2025-11-03 18:38:37 +0900

    feat(LottoController): 로또 구매

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.model.BonusNumber;
 import lotto.model.Lotto;
@@ -10,12 +11,28 @@ import lotto.view.InputView;
 public class LottoController {
     private List<Lotto> lottos;
 
+    LottoController() {
+        lottos = new ArrayList<>();
+    }
+
     public void run() {
         Money money = getMoney();
+        purchaseLottos(calculatePurchaseCount(money.getMoney()));
         PrizeNumber prizeNumber = getPrizeNumber();
         BonusNumber bonusNumber = getBonusNumber(prizeNumber);
 
         InputView.close();
+    }
+
+    private void purchaseLottos(int count) {
+        for (int i = 0; i < count; i++) {
+            Lotto lotto = Lotto.createLotto();
+            lottos.add(lotto);
+        }
+    }
+
+    private int calculatePurchaseCount(int amount) {
+        return amount / 1000;
     }
 
     private Money getMoney() {
