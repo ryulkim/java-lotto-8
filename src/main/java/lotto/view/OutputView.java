5ae7507dc1fa2d1e1ed368ff4ec49a4c132d139a
ryulkim <hobby519@naver.com>
2025-11-03 20:19:42 +0900

    fix(OutputView): 당첨 통계 오류 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -20,11 +20,21 @@ public class OutputView {
 
         StringBuilder sb = new StringBuilder();
         sb.append("당첨 통계").append("\n").append("---").append("\n");
-        for (int i = 3; i <= 6; i++) {
-            sb.append(i).append("개 일치 (").append(String.format("%,d", winnerPrize[i])).append("원) - ")
+        for (int i = 3; i <= 7; i++) {
+            sb.append(printCount(i)).append(String.format("%,d", winnerPrize[i])).append("원) - ")
                     .append(correctCount[i]).append("개").append("\n");
         }
-        sb.append("총 수익률은 ").append(String.format("%2d", rate)).append("%입니다.").append("\n");
+        sb.append("총 수익률은 ").append(String.format("%.1f", rate)).append("%입니다.").append("\n");
         Print.printString(sb.toString());
+    }
+
+    private static String printCount(int num) {
+        if (num == 6) {
+            return "5개 일치, 보너스 볼 일치 (";
+        }
+        if (num == 7) {
+            return "6개 일치 (";
+        }
+        return String.format("%d개 일치 (", num);
     }
 }
